01342d328d44c5d77a5cc1534dedd446e275a455
agcoelho <afonsogcoelho@gmail.com>
2024-07-31 19:50:43 -0400

    escondendo informações sigilosas

--- cypress/e2e/api_mernsagens.cy.js
+++ cypress/e2e/api_mernsagens.cy.js
@@ -1,18 +1,17 @@
 describe('Api Adopet', ()=>{
-    const tempoEpserado = Math.random() * 1000
-    const authorization = ' Bearer [aqui deve vir o token]'
+    //const tempoEpserado = Math.random() * 1000
 
     it('Mensagens da API', ()=>{
         cy.request({
             method: 'GET' ,
             url:'[url de quem está fazendo a requisição' ,
-            headers: {authorization},
+            headers: Cypress.env(),
 
         }).then((res)=>{
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('msg')
-            expect(res.duration).to.be.lte(tempoEpserado)
+            //expect(res.duration).to.be.lte(tempoEpserado) //flaky test
 
         })
 
