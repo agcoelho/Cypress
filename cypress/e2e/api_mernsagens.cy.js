0b78e74299404a924431a1d55cdef07bdc1b29ce
agcoelho <afonsogcoelho@gmail.com>
2024-07-31 19:44:16 -0400

    adicionando requisição de api

---
+++ cypress/e2e/api_mernsagens.cy.js
@@ -0,0 +1,21 @@
+describe('Api Adopet', ()=>{
+    const authorization = ' Bearer [aqui deve vir o token]'
+
+    it('Mensagens da API', ()=>{
+        cy.request({
+            method: 'GET' ,
+            url:'[url de quem está fazendo a requisição' ,
+            headers: {authorization},
+
+        }).then((res)=>{
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('msg')
+
+        })
+
+
+    })
+
+
+})
