b1116fc5756bf023314d6ba84111585408525299
agcoelho <afonsogcoelho@gmail.com>
2024-07-31 18:28:37 -0400

    integração com cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "hob7fj",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     video: true,
     reporter: 'mochawesome',
-    reportDir: 'cypress/results',
-    overwrite: false,
-    json: true,
-    html: true,
-    timestamp: 'mmddyyyy_HHMMss'
-  },
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      json: false,
+      html: true,
+      timestamp: 'mmddyyyy_HHMMss'
+    }},
 });
